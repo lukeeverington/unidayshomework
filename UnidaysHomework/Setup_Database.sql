07e14750a890d22a85112f439023eb0068332226
Luke Everington <l@ukeeverington.uk>
2018-05-20 19:42:49 +0100

    Added sql scripts to setup user credentials for website

--- UnidaysHomework/Setup_Database.sql
+++ UnidaysHomework/Setup_Database.sql
@@ -8,3 +8,12 @@ CREATE TABLE [User] (
 	PasswordHash NVARCHAR(60),
 	PRIMARY KEY (Id)
 )
+
+CREATE LOGIN UniDaysHomeworkWebsite 
+WITH PASSWORD = 'f95a976169a3818c21f84ada5c2e74aa2cb5b08da58397d0f85e80f256e3fff9'
+
+CREATE USER UniDaysHomeworkWebsite 
+FOR LOGIN UniDaysHomeworkWebsite
+
+GRANT SELECT TO UniDaysHomeworkWebsite
+GRANT INSERT TO UniDaysHomeworkWebsite
